bd5635057e385d9b71c34ad31e1ff0f3b83e52a9
Umut Dinçer Yananer <umutdncr@gmail.com>
2024-10-21 20:58:52 +0300

    Installed Vite and Initialized Header

---
+++ frontend/petlyst-webapp-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/layout/Header';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Header />
+      <main>
+        <p>Petlyst uygulamasına hoş geldiniz!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
