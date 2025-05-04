ca4b82ccdc7cc66bb988c6e9ac31e8da54f3668e
Umut Dinçer Yananer <umutdncr@gmail.com>
2025-05-04 21:59:06 +0300

    Backend Port Changed 3001 and PC Change

--- backend/petlyst-webapp-backend/server.js
+++ backend/petlyst-webapp-backend/server.js
@@ -186,7 +186,7 @@ pool.query(`
     console.error('Error checking database schema:', err);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
